bd60db4501d72d8acb2ffc9fd0fbdc09b75b4619
serjo <serjo-study@ya.ru>
2025-10-27 16:21:41 +0300

    feat: add new task

---
+++ checker-size-img/index.js
@@ -0,0 +1,42 @@
+// import express from 'express';
+// import multer from 'multer';
+// import sharp from 'sharp';
+
+const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
+// const https = require('https');
+// const fs = require('fs');
+
+const app = express();
+const upload = multer(); // сохраняем в оперативной памяти
+
+const LOGIN = "serjo4110";
+
+app.get('/login', (req, res) => {
+    res.type('text/plain').send(LOGIN);
+});
+
+app.post("/size2json", upload.single("image"), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: "Не передано поле image" });
+        }
+
+        const metadata = await sharp(req.file.buffer).metadata();
+
+        res.json({
+            width: metadata.width,
+            height: metadata.height
+        });
+    } catch (err) {
+        res.status(500).json({ error: "Ошибка обработки изображения" });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
+
+export default app;
