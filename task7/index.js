e989a0f2904f2900a2874e366fbbc70e35ee0b22
serjo <serjo-study@ya.ru>
2025-11-28 10:57:42 +0300

    feat: add task7

---
+++ task7/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { MongoClient } = require('mongoose');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/login/', (_, res) => {
+  // TODO: Добавьте ваш логин
+  res.send('b8d44289-d86a-471b-9f1d-aceec5c9e948');
+});
+
+app.post('/insert/', async (req, res) => {
+  let client;
+
+  try {
+    const { login, password, URL } = req.body;
+
+    client = new MongoClient(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    await client.connect();
+
+    // Get DB from URL -> "readusers"
+    const dbName = URL.split('/').pop().split('?')[0];
+    const db = client.db(dbName);
+
+    const usersCollection = db.collection('users');
+
+    const userDocument = {
+      login: login,
+      password: password,
+      createdAt: new Date()
+    };
+
+    await usersCollection.insertOne(userDocument);
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.sendStatus(500);
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+});
+
+const PORT = 3000; // Изменили порт на стандартный для HTTP
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
