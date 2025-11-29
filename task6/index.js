68bec9cf3af4f150b2eb48286dbb0870799b88ea
serjo <serjo-study@ya.ru>
2025-11-29 15:41:54 +0300

    feat: add task6

---
+++ task6/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createReadStream } from "fs";
+import crypto from "crypto";
+import http from "http";
+import bodyParser from "body-parser";
+import { createApp } from "./app.js";
+
+const app = createApp(express, bodyParser, createReadStream, crypto, http);
+
+const PORT = 3000;
+
+app.listen(PORT);
+
+export default app;
